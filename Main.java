74f750bf87c53e0c60a9f968dfc202d717cafcd8
Joeliii <joelboy12704@gmail.com>
2024-02-14 13:52:00 -0500

    Finished Lab4

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
-/** Programmers:  [your names here]
+/** Programmers:  Costa Marmaras, Joel Robinson
 * Course:  CS 212
-* Due Date:
-* Lab Assignment:
-* Problem Statement:
-* Data In:
-* Data Out:
+* Due Date: 2/18/24
+* Lab Assignment: 4
+* Problem Statement: Create a class that will create sphere objects with coordinates and a radius, and calculate the surface area, the volume,
+ * the distance from a point, and if two spheres will collide
+* Data In: coordinates for distance from a point
+* Data Out: Surface area, Volume, Distance from a point, if two spheres will collide
 * Credits: [Is your code based on an example in the book, in class, or something else?
 *            Reminder: you should never take code from the Internet or another person
 */
@@ -13,7 +14,63 @@ import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
        System.out.println("Music of the Spheres");
+       Sphere sun = new Sphere(0.0, 0.0, 0.0, 43.26);
+       Sphere earth = new Sphere(26.0, 42.0, 79.0, 0.395);
+       Sphere planetX = new Sphere(25.3, 43.1, 77.8, 1.4);
+
+        System.out.println("Input the x Coordinate of Your planet:");
+        while(!input.hasNextDouble()){
+            System.out.println("Please choose a double.");
+            input.next();
+        }
+        double x = input.nextDouble();
+        System.out.println("Input the y Coordinate of Your Planet:");
+        while(!input.hasNextDouble()){
+            System.out.println("Please choose a double.");
+            input.next();
+        }
+        double y = input.nextDouble();
+        System.out.println("Input the z Coordinate of Your Planet:");
+        while(!input.hasNextDouble()){
+            System.out.println("Please choose a double.");
+            input.next();
+        }
+        double z = input.nextDouble();
+        System.out.println("Input the Radius of Your Planet:");
+        while(!input.hasNextDouble()){
+            System.out.println("Please choose a double.");
+            input.next();
+        }
+        double radius = input.nextDouble();
+        Sphere userPlanet = new Sphere(x, y, z, radius);
+
+        System.out.println("Surface Area of The Sun: " + sun.surfaceArea() + "\nVolume of The Sun: " + sun.Volume());
+        System.out.println("Surface Area of the Earth: " + earth.surfaceArea() + "\nVolume of the Earth:" + earth.Volume());
+        System.out.println("Surface Area of Planet X: " + planetX.surfaceArea() + "\nVolume of Planet X: " + planetX.Volume());
+        System.out.println("Surface Area of User Created Planet: " + userPlanet.surfaceArea() + "\nVolume of User Created Planet: " + userPlanet.Volume());
+        sun.SphereCollide(sun, earth);
+        sun.SphereCollide(sun, planetX);
+        sun.SphereCollide(earth, planetX);
+        userPlanet.SphereCollide(userPlanet, planetX);
+        userPlanet.SphereCollide(userPlanet, earth);
+        userPlanet.SphereCollide(userPlanet,sun);
+
+        double distfromSun = userPlanet.DistanceFromPoint(sun);
+        double distfromEarth = userPlanet.DistanceFromPoint(earth);
+        double distfromX = userPlanet.DistanceFromPoint(planetX);
+
+        if(distfromEarth < distfromX && distfromEarth < distfromSun){
+            System.out.println("You are closest to the Earth.");
+        }
+        if(distfromX < distfromEarth && distfromX < distfromSun){
+            System.out.println("You are closest to the Planet X.");
+        }
+        if(distfromSun < distfromEarth && distfromSun < distfromX){
+            System.out.println("You are closest to the Sun.");
+        }
+
 
 
     }
--- Sphere.java
+++ Sphere.java
@@ -10,11 +10,60 @@
 *            Reminder: you should never take code from the Internet or another person
 */
 
+import java.util.Scanner;
 
 class Sphere {
+    Scanner question = new Scanner(System.in);
+    private double x;
+    private double y;
+    private double z;
+    private double radius;
+
+    public Sphere(){
+        x = 0;
+        y = 0;
+        z = 0;
+        radius = 0;
+    }
+    public Sphere(double x, double y, double z, double radius){
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+
+    }
+
+     double surfaceArea(){
+        double area = 4 * Math.PI * radius;
+        return area;
+    }
+     double Volume(){
+        double volume = (4/3) * Math.PI * Math.pow(radius,3);
+        return volume;
+    }
+    double DistanceFromPoint(Sphere circ1){
+        double distance = Math.sqrt(Math.pow(x-circ1.x,2) + Math.pow(y-circ1.y,2) + Math.pow(z-circ1.z,2));
+        System.out.println("The distance from this point is: " + distance);
+        return distance;
+
+    }
+
+    void SphereCollide(Sphere circ1, Sphere circ2)
+    {
+        if(circ1.DistanceFromPoint(circ2) < (circ1.radius + circ2.radius))
+        {
+            System.out.println("The Spheres Collided");
+        }
+        else
+        {
+            System.out.println("The Spheres Did Not Collide");
+        }
+    }
+
     public static void main(String[] args) {
        System.out.println("Spheres Go Here");
 
 
     }
+
 }
